f32c3c34179b543d2dbd47ae7464c65017330036
Enzo Lizama <lizama.enzo@gmail.com>
2024-10-28 12:17:48 -0500

    remove imports

--- lib/ui/speakers_list_view.dart
+++ lib/ui/speakers_list_view.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutterconflatam_accesibility/data/speakers_repository.dart';
 import 'package:flutterconflatam_accesibility/models/speaker.dart';
